90a816053a1e85bc7a93782fb816d965bc7dd60c
azalinux <52433182+azalinux@users.noreply.github.com>
2020-05-31 11:44:44 +1000

    Update deepin_setup.sh

--- deepin_setup.sh
+++ deepin_setup.sh
@@ -11,7 +11,7 @@ sudo pacman -S --noconfirm --needed arch-install-scripts
 sudo pacman -S --noconfirm --needed os-prober
 sudo pacman -S --noconfirm --needed openssh
 sudo systemctl enable sshd
-sudo pacman -S --noconfirm --needed dhcpcd
+#sudo pacman -S --noconfirm --needed dhcpcd
 sudo pacman -S linux-headers --noconfirm --needed
 
 sudo pacman -S deepin deepin-extra --noconfirm --needed
