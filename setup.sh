a25a8856d8a45590a6b1595c8134070da735df40
Azalinux <52433182+azalinux@users.noreply.github.com>
2020-09-12 15:35:27 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash -x
 set -e
 set -u
@@ -26,7 +27,7 @@ echo -n "127.0.1.1 myhostname.localdomain myhostname" >> /etc/hosts
 
 echo "######## Check what hard drive you are installing on for grub ie sda, sdb ... and edit the 'grub-install' line !!!! #####"
 echo "########      Default is sda      ############"
-sleep 5
+sleep 3
 passwd
 useradd -m aza
 passwd aza
@@ -34,14 +35,25 @@ usermod -aG wheel,audio,video,optical,storage,sys aza
 #pacman -S --noconfirm --needed git
 sh reflector.sh
 pacman -S --noconfirm --needed xdg-user-dirs
+pacman -S --noconfirm --needed nano
 pacman -S --noconfirm --needed grub
-grub-install /dev/sda
+pacman -S --noconfirm --needed dosfstools
+pacman -S --noconfirm --needed efibootmgr
+# FOR UEFI create /efi partition - FAT32 - 512Mb
+#mkfs.fat -F32 /dev/sdb1
+# You have to mount the EFI partition then create the 'efi' directory on it!
+mkdir /efi
+mount /dev/sda5 /efi
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+
+#grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 pacman -S --noconfirm --needed base-devel
 pacman -S --noconfirm --needed vim
 # visudo [uncomment "Wheel root access"] (to save type: :wq )
 echo " visudo is about to run - uncomment the 'wheel root access' then type ':wq' to save and exit :)"
-sleep 5
+sleep 3
 visudo
 pacman -S --noconfirm --needed dhcpcd
 systemctl enable dhcpcd
+#sudo systemctl enable NetworkManager
